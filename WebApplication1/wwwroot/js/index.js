7f6623f9f6a26f240d69af2e2c5efe7ef483f6a1
Hamnah Hussain <hamnah-hussain@hotmail.com>
2022-10-27 10:54:12 +0200

    packages

--- WebApplication1/wwwroot/js/index.js
+++ WebApplication1/wwwroot/js/index.js
@@ -1,6 +1,40 @@
-﻿namespace WebApplication1.wwwroot.js
-{
-    public class index
-    {
-    }
+﻿
+$(function() {
+    hentAlleKunder();
+});
+
+function hentAlleKunder() {
+    $.get("Database/HentAlle", function (diagnoser) {
+        formaterKunder(diagnoser);
+    });
 }
+
+function formaterKunder(kunder) {
+    let ut = "<table class='table table-striped'>" +
+        "<tr>" +
+        "<th>Navn</th><th>Adresse</th><th></th><th></th>" +
+        "</tr>";
+    for (let kunde of kunder) {
+        ut += "<tr>" +
+            "<td>" + kunde.navn + "</td>" +
+            "<td>" + kunde.adresse + "</td>" +
+            "<td> <a class='btn btn-primary' href='endre.html?id=" + kunde.id + "'>Endre</a></td>" +
+            "<td> <button class='btn btn-danger' onclick='slettKunde(" + kunde.id + ")'>Slett</button></td>" +
+            "</tr>";
+    }
+    ut += "</table>";
+    $("#kundene").html(ut);
+}
+
+function slettKunde(id) {
+    const url = "Kunde/Slett?id=" + id;
+    $.get(url, function (OK) {
+        if (OK) {
+            window.location.href = 'index.html';
+        }
+        else {
+            $("#feil").html("Feil i db - prøv igjen senere");
+        }
+
+    });
+};
